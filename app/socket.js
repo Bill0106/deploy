21bdd64ac039644fe149779749f7a8dbf97d7586
Bill0106 <bill2010111@gmail.com>
2017-07-25 07:46:46 +0000

    log style

--- app/socket.js
+++ app/socket.js
@@ -1,7 +1,9 @@
 const child = require('child_process')
 const socketIO = require('socket.io')
+const AU = require('ansi_up')
 const Commits = require('./models/CommitsModel')
 const Logs = require('./models/LogsModel')
+const ansi_up = new AU.default
 
 const socket = (server) => {
   const io = socketIO(server)
@@ -9,20 +11,16 @@ const socket = (server) => {
   io.on('connection', (socket) => {
     socket.on('build', (data) => {
       const { id } = data
-      console.log(id)
       Commits.findById(id, (err, res) => {
         if (err) {
           socket.emit('error', err.message)
         }
 
-        console.log(res.repo)
         socket.emit('log', 'start building...')
         const spawn = child.spawn('bash', ['/var/www/build.sh', res.repo, res.commit_id])
         spawn.stdout.on('data', (data) => {
-          console.log(data)
-          socket.emit('log', data.toString())
+          socket.emit('log', ansi_up.ansi_to_html(data.toString()))
         })
-        spawn.stderr.on('data', (data) => socket.emit('error', data.toString()))
       })
     })
   })
