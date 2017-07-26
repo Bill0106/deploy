b4c1326bc61a0ae9677f7ca980219b8cba81bca2
Bill0106 <bill2010111@gmail.com>
2017-07-26 05:45:13 +0000

    routes

--- server.js
+++ server.js
@@ -3,21 +3,13 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
 const bluebird = require('bluebird')
-const routes = require('./app')
+const routes = require('./app/routes')
 const app = express()
 const server = require('http').Server(app)
 require('./app/socket')(server)
 
 mongoose.Promise = bluebird
 mongoose.connect('mongodb://localhost/code')
-
-mongoose.connection.on('connected', function () {
-  console.log('Database is connected');
-})
-
-mongoose.connection.on('error',function (err) {
-  console.log('Mongoose default connection error: ' + err);
-})
 
 app.set('views', path.join(__dirname, '/app'))
 app.set('view engine', 'pug')
