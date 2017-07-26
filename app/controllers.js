5a172644ae8b794046df502600edaedebc2e8173
Bill0106 <bill2010111@gmail.com>
2017-07-26 07:01:11 +0000

    commits list switch

--- app/controllers.js
+++ app/controllers.js
@@ -1,4 +1,5 @@
 const crypto = require('crypto')
+const moment = require('moment')
 const token = require('./token')
 const Commits = require('./models/CommitsModel')
 
@@ -7,11 +8,34 @@ const index = (req, res) => {
   const limit = 30
   const page = Number(req.query.page) || 1
   const offset = limit * (page - 1)
-  Commits.where('repo', repo).limit(limit).skip(offset).find((err, commits) => {
-    if (err) res.sendStatus(500)
 
-    res.render('commits', { commits })
-  })
+  Commits.where('repo', repo).limit(limit).skip(offset).sort({ committedAt: 'desc' }).find().exec()
+    .then(data => {
+      const commits = data.map(item => {
+        let dist_files = ''
+        if (item.dist_files) {
+          const files = JSON.parse(item.dist_files)
+          dist_files = Object.keys(files).map(item => files[item]).join(', ')
+        }
+
+        return {
+          _id: item._id,
+          commit_id: item.commit_id,
+          commit_message: item.commit_message,
+          committer_name: item.committer_name,
+          committer_email: item.committer_email,
+          committedAt: moment(item.committedAt).format('YYYY-MM-DD hh:mm:ss'),
+          published: item.published,
+          dist_files,
+        }
+      })
+
+      res.render('commits', { commits, repo })
+    })
+    .catch(err => {
+      console.log(err)
+      res.sendStatus(500)
+    })
 }
 
 const webhook = (req, res) => {
