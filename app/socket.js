c6895ddf668681067bcd9e7bf3f8ac26a2ee2a38
Bill0106 <bill2010111@gmail.com>
2017-07-26 03:45:21 +0000

    save dist file

--- app/socket.js
+++ app/socket.js
@@ -1,9 +1,31 @@
+const fs = require('fs')
+const path = require('path')
 const child = require('child_process')
 const socketIO = require('socket.io')
 const AU = require('ansi_up')
 const Commits = require('./models/CommitsModel')
 const Logs = require('./models/LogsModel')
 const ansi_up = new AU.default
+
+const build = (commit, socket) => {
+  let log = ''
+  const spawn = child.spawn('bash', ['/var/www/build.sh', commit.repo, commit.commit_id])
+
+  spawn.stdout.on('data', data => {
+    log += ansi_up.ansi_to_html(data.toString())
+    socket.emit('log', log)
+  })
+
+  spawn.on('close', code => {
+    Logs.create({ commit_id: commit._id, log })
+
+    const manifest = path.join(__dirname, '../', 'manifest.json')
+    commit.dist_files = fs.readFileSync(manifest).toString()
+    commit.save()
+    fs.unlinkSync(manifest)
+    socket.emit('finish', true)
+  })
+}
 
 const socket = server => {
   const io = socketIO(server)
@@ -23,19 +45,11 @@ const socket = server => {
         })
         .then(commit => {
           if (!commit) {
+            socket.emit('error', 'No such commit!')
             return false
           }
 
-          let log = ''
-          socket.emit('log', log)
-          const spawn = child.spawn('bash', ['/var/www/build.sh', commit.repo, commit.commit_id])
-          spawn.stdout.on('data', data => {
-            log += ansi_up.ansi_to_html(data.toString())
-            socket.emit('log', log)
-          })
-          spawn.on('close', code => {
-            Logs.create({ commit_id: commit._id, log })
-          })
+          build(commit, socket)
         })
         .catch(err => console.log(err))
     })
