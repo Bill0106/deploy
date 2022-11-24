4520cffee3436ea8922d7bf89a8f1a6cf87e6179
Bill <bill2010111@gmail.com>
2022-11-24 18:48:25 +0800

    feat: axios

--- .github/scripts/start.js
+++ .github/scripts/start.js
@@ -1,31 +1,16 @@
-const https = require('follow-redirects').https;
+const axios = require('axios')
 
 module.exports = method => {
   const { VERSION_ID, PAT, GITHUB_RUN_ID } = process.env
-  const options = {
-    'method': 'POST',
-    'hostname': 'api.zhuhaolin.com',
-    'path': `/v1/github/workflow/_${method}`,
-    'headers': {
+  const config = {
+    method: 'post',
+    url: `api.zhuhaolin.com/v1/github/workflow/_${method}`,
+    headers: {
       'pat': PAT,
       'Content-Type': 'application/json'
     },
-    'maxRedirects': 20
+    data: JSON.stringify({ id: VERSION_ID, runId: GITHUB_RUN_ID })
   };
 
-  return new Promise((resolve, reject) => {
-    const req = https.request(options, res => {    
-      res.on('end', () => {
-        resolve()
-      });
-    
-      res.on('error', error => {
-        reject(error)
-      });
-    });
-    
-    req.write(JSON.stringify({ id: VERSION_ID, runId: GITHUB_RUN_ID }));
-    
-    req.end();
-  })
+  return axios(config)
 }
